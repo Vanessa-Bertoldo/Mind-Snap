ff2508ee68f0943ae86effacb5cbe30b96b22363
vanessa <vanessa.bert311@gmail.com>
2024-01-20 19:28:12 -0300

    insert form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { CreateThoughtComponent } from './component/thought/create-thought/create-thought.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    CreateThoughtComponent,
   ],
   imports: [
     BrowserModule,
